1f1e26b7cbc3f5ff203f8954842da99564e3a59e
Jeff Rotar <238284064+JROFND@users.noreply.github.com>
2026-07-21 15:22:10 -0500

    Update cleaning.py

--- cleaning.py
+++ cleaning.py
@@ -7,13 +7,12 @@ import numpy as np
 import re
 warnings.filterwarnings("ignore", category=UserWarning, module="openpyxl")
 
-save_folder = r'W:\SHARE2 Pharmacy Department Share\340B\Jeff R\Specialty Pharmacy Tool'
+save_folder = r'W:'
 os.chdir(save_folder)
 
 
 # Find all Excel files matching the pattern
-cardinal_files = glob.glob(os.path.join(save_folder, 'SANFORD_*.xlsx'))
-apexus_files = glob.glob(os.path.join(save_folder, 'APEXUS*.xlsx'))
+files = glob.glob(os.path.join(save_folder, '_*.xlsx'))
 
 
 df_list = []
@@ -36,7 +35,7 @@ def standardize_ndc(ndc):
 # Process Cardinal files
 
 
-for file_path in cardinal_files:
+for file_path in files:
     df = pd.read_excel(
         file_path,  engine='openpyxl',
         usecols=['NDC 11', 'NDC Number',
@@ -56,12 +55,12 @@ for file_path in cardinal_files:
     )
     df_list.append(df)
 
-cardinal_df = pd.concat(df_list, ignore_index=True)
+df = pd.concat(df_list, ignore_index=True)
 
-cardinal_df_copy = cardinal_df.copy()
+df_copy = cardinal_df.copy()
 
-cardinal_df_copy = cardinal_df_copy[cardinal_df_copy['Account'].isin(
-    ['2150124178', '2150125178', '2150125169', '2150125128'])]
+df_copy = df_copy[df_copy['Account'].isin(
+    [''])]
 
 cardinal_df_copy['NDC_Number'] = cardinal_df_copy['NDC_Number'].apply(
     standardize_ndc)
@@ -69,14 +68,13 @@ cardinal_df_copy['NDC_Number'] = cardinal_df_copy['NDC_Number'].apply(
     
 # Process Apexus file
 apexus_df = pd.read_excel(apexus_files[0], engine='openpyxl', sheet_name='Non-GPO_WAC_PVP_catalog',header=3, 
-                          usecols=['NDC', 'Item #', '340B Selling Price', '340B Selling Price Eff Date']).rename(
-    columns={'340B Selling Price': 'Ceiling_Price', '340B Selling Price Eff Date': 'Ceiling_Date'})
+                          usecols=[{'Item', 'Price', 'Date'})
 
 apexus_df_copy = apexus_df.copy()
 
 
 apexus_df_copy = apexus_df_copy[
-    apexus_df_copy['NDC'].notna() &
+    apexus_df_copy['ITEM'].notna() &
     ~apexus_df_copy['Item #'].str.contains('DPPV', case=False, na=False)
 ].reset_index()
 
